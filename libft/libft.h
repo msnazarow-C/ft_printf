6140e39cb3e4c8112fccf4353d941d2fef4ebe6a
msnazarow <60715299+msnazarow@users.noreply.github.com>
2020-05-24 13:46:37 +0300

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -56,13 +56,13 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 t_list			*ft_lstnew(void *content);
-void			ft_lstadd_front(t_list **list, t_list *new);
-void			ft_lstadd_back(t_list **list, t_list *new);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+void			ft_lstadd_back(t_list **lst, t_list *new);
 void			ft_lstclear(t_list **lst, void (*del)(void*));
 void			ft_lstdelone(t_list *lst, void (*del)(void*));
-int				ft_lstsize(t_list *list);
-t_list			*ft_lstlast(t_list *list);
-void			ft_lstiter(t_list *list, void (*f)(void *));
+int				ft_lstsize(t_list *lst);
+t_list			*ft_lstlast(t_list *lst);
+void			ft_lstiter(t_list *lst, void (*f)(void *));
 t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
 			void (*del)(void *));
 #endif
