4e41c7a6c5115a33c0b99fd55b2c791894ef242f
msnazarow <60715299+msnazarow@users.noreply.github.com>
2020-05-24 10:32:56 +0300

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -48,7 +48,7 @@ void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			*ft_calloc(size_t nmemb, size_t size);
 char			*ft_strdup(const char *s);
-char			*ft_strnstr(char *str, char *to_find, size_t len);
+char			*ft_strnstr(const char *str, const char *to_find, size_t len);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
